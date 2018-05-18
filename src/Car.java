afba6668884a690734dfebcc15255c9cd91e57e4
sarperkumcu <s.sarperkumcu@gmail.com>
2018-05-18 23:48:53 +0300

    car added and bugs fixed

---
+++ src/Car.java
@@ -0,0 +1,122 @@
+
+public class Car {
+	private Integer carID;
+	private String carBrand;
+	private String carModel;
+	private Integer transmissionTypeID;
+	private Integer fuelTypeID;
+	private Integer colorID;
+	private String transmissionType;
+	private String fuelType;
+	private String colorName;
+	/**
+	 * @return the carID
+	 */
+	public Integer getCarID() {
+		return carID;
+	}
+	/**
+	 * @param carID the carID to set
+	 */
+	public void setCarID(Integer carID) {
+		this.carID = carID;
+	}
+	/**
+	 * @return the carBrand
+	 */
+	public String getCarBrand() {
+		return carBrand;
+	}
+	/**
+	 * @param carBrand the carBrand to set
+	 */
+	public void setCarBrand(String carBrand) {
+		this.carBrand = carBrand;
+	}
+	/**
+	 * @return the carModel
+	 */
+	public String getCarModel() {
+		return carModel;
+	}
+	/**
+	 * @param carModel the carModel to set
+	 */
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	/**
+	 * @return the transmissionTypeID
+	 */
+	public Integer getTransmissionTypeID() {
+		return transmissionTypeID;
+	}
+	/**
+	 * @param transmissionTypeID the transmissionTypeID to set
+	 */
+	public void setTransmissionTypeID(Integer transmissionTypeID) {
+		this.transmissionTypeID = transmissionTypeID;
+	}
+	/**
+	 * @return the transmissionFuelTypeID
+	 */
+	public Integer getFuelTypeID() {
+		return fuelTypeID;
+	}
+	/**
+	 * @param transmissionFuelTypeID the transmissionFuelTypeID to set
+	 */
+	public void setFuelTypeID(Integer fuelTypeID) {
+		this.fuelTypeID = fuelTypeID;
+	}
+	/**
+	 * @return the colorID
+	 */
+	public Integer getColorID() {
+		return colorID;
+	}
+	/**
+	 * @param colorID the colorID to set
+	 */
+	public void setColorID(Integer colorID) {
+		this.colorID = colorID;
+	}
+	/**
+	 * @return the transmissionType
+	 */
+	public String getTransmissionType() {
+		return transmissionType;
+	}
+	/**
+	 * @param transmissionType the transmissionType to set
+	 */
+	public void setTransmissionType(String transmissionType) {
+		this.transmissionType = transmissionType;
+	}
+	/**
+	 * @return the fuelType
+	 */
+	public String getFuelType() {
+		return fuelType;
+	}
+	/**
+	 * @param fuelType the fuelType to set
+	 */
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	/**
+	 * @return the colorName
+	 */
+	public String getColorName() {
+		return colorName;
+	}
+	/**
+	 * @param colorName the colorName to set
+	 */
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	
+	
+}
--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -5,8 +5,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
+
 import java.sql.Driver;
 public class DatabaseConnector {
+    Connection conn = null;
 
     public Connection connectDatabase(){
     // JDBC driver name and database URL
@@ -21,7 +25,6 @@ public class DatabaseConnector {
     final String USER = "root";
      final String PASS = "sarper";
 
-        Connection conn = null;
         //Statement stmt = null;
         try {
             //STEP 2: Register JDBC driver
@@ -43,7 +46,6 @@ public class DatabaseConnector {
             //Handle errors for Class.forName
             e.printStackTrace();
         } 
-        System.out.println("Goodbye!");
         return conn;
 }
     public void createTables(Connection connection) {
@@ -140,62 +142,72 @@ public class DatabaseConnector {
     	
     	String initYakitTuru = 
     			"INSERT IGNORE INTO Tbl_YakitTuru" 
-    		+	"(YakitTuruID, YakitTuru)"
+    		+	"(YakitTuru)"
     		+	"VALUES"
-    		+	"(0,'Diesel'),"
-    		+	"(1,'Gasoline'),"
-    		+	"(2,'LPG'),"
-    		+	"(3,'Electric'),"
-    		+	"(4,'Faa')";
+    		+	"('Diesel'),"
+    		+	"('Gasoline'),"
+    		+	"('LPG'),"
+    		+	"('Electric')";
      	String initVitesTuru = 
      			"INSERT IGNORE INTO Tbl_VitesTuru" 
-     		+	"(VitesTuruID, VitesTuru)"
+     		+	"(VitesTuru)"
      		+	"VALUES"
-     		+	"(0,'Manual'),"
-     		+	"(1,'Automatic')";
+     		+	"('Manual'),"
+     		+	"('Half Automatic'),"
+     		+	"('Automatic')";
     	
     	String initRenk =
     			"INSERT IGNORE INTO Tbl_Renk" 
-    		+	"(RenkID, Renk)"
+    		+	"( Renk)"
     		+	"VALUES"
-      		+	"(0,'Black'),"
-    		+	"(1,'White'),"
-    		+	"(2,'Red'),"
-    		+	"(3,'Blue'),"
-    		+	"(4,'Green'),"
-    		+	"(5,'Yellow'),"
-    		+	"(6,'Orange'),"
-    		+	"(7,'Purple'),"
-    		+	"(8,'Navy Blue'),"
-    		+	"(9,'Gray'),"
-    		+	"(10,'Pink'),"
-    		+	"(11,'Turkuaz'),"
-    		+	"(12,'Fuchsia')";
+      		+	"('Black'),"
+    		+	"('White'),"
+    		+	"('Red'),"
+    		+	"('Blue'),"
+    		+	"('Green'),"
+    		+	"('Yellow'),"
+    		+	"('Orange'),"
+    		+	"('Purple'),"
+    		+	"('Navy Blue'),"
+    		+	"('Gray'),"
+    		+	"('Pink'),"
+    		+	"('Turkuaz'),"
+    		+	"('Fuchsia')";
 
 
     	String initSehir =
-    			"INSERT IGNORE INTO Tbl_Renk" 
-    		    +	"(RenkID, Renk)"
+    			"INSERT IGNORE INTO Tbl_Sehir" 
+    		    +	"(Sehir)"
     		    +	"VALUES"
-    		    +	"(1,'Adana'),"
-    		    +	"(2,'Adiyaman'),"
-    		    +	"(3,'Afyon'),"
-    		    +	"(4,'Agri'),"
-    		    +	"(5,'Amasya'),"
-    		    +	"(6,'Ankara'),"
-    		    +	"(7,'Antalya'),"
-    		    +	"(8,'Artvin'),"
-    		    +	"(9,'Aydin'),"
-    		    +	"(10,'Balikesir'),"
-    		    +	"(11,'Bilecik'),"
-    		    +	"(12,'Bingol')";
-    	
+    		    +	"('Adana'),"
+    		    +	"('Adiyaman'),"
+    		    +	"('Afyon'),"
+    		    +	"('Agri'),"
+    		    +	"('Amasya'),"
+    		    +	"('Ankara'),"
+    		    +	"('Antalya'),"
+    		    +	"('Artvin'),"
+    		    +	"('Aydin'),"
+    		    +	"('Balikesir'),"
+    		    +	"('Bilecik'),"
+    		    +	"('Bingol')";
+    	String initAraba =
+    			"INSERT INTO Tbl_Araba" 
+    		    +	"(Araba_Marka,Araba_Model,Araba_VitesTuruID,Araba_YakitTuruID,Araba_RenkID)"
+    		    +	"VALUES"
+    		    +	"('Mercedes - Benz','C 200 D BlueTEC',(SELECT VitesTuruID from Tbl_VitesTuru WHERE VitesTuru = 'Half Automatic'),(SELECT YakitTuruID from Tbl_YakitTuru WHERE YakitTuru = 'Diesel'),(SELECT RenkID from Tbl_Renk WHERE Renk = 'Black')),"
+    		    +	"('Mercedes - Benz','E 400 CDI Avantgarde',(SELECT VitesTuruID from Tbl_VitesTuru WHERE VitesTuru = 'Automatic'),(SELECT YakitTuruID from Tbl_YakitTuru WHERE YakitTuru = 'Gasoline'),(SELECT RenkID from Tbl_Renk WHERE Renk = 'Blue')),"
+    		    +	"('Mercedes - Benz','C180 Exclusivea',(SELECT VitesTuruID from Tbl_VitesTuru WHERE VitesTuru = 'Manual'),(SELECT YakitTuruID from Tbl_YakitTuru WHERE YakitTuru = 'Gasoline'),(SELECT RenkID from Tbl_Renk WHERE Renk = 'Red')),"
+    		    +	"('Mercedes - Benz','C 200d AMG',(SELECT VitesTuruID from Tbl_VitesTuru WHERE VitesTuru = 'Automatic'),(SELECT YakitTuruID from Tbl_YakitTuru WHERE YakitTuru = 'LPG'),(SELECT RenkID from Tbl_Renk WHERE Renk = 'Red'))";
+    	   	
     	
     	try {
 			statement = connection.createStatement();
 			statement.executeUpdate(initYakitTuru);
 			statement.executeUpdate(initVitesTuru);
 			statement.executeUpdate(initRenk);
+			statement.executeUpdate(initSehir);
+			statement.executeUpdate(initAraba);
 		
 			System.out.println("All Fine.");
 			
@@ -216,20 +228,20 @@ public class DatabaseConnector {
 
 		}
     }
-    public void selectQuery(String tableName,Connection conn) {
+    public ArrayList<Object> selectAllQuery(String tableName,Connection conn) {
     	String query;
-    	query = "SELECT * FROM "+ tableName;
+    	query = "SELECT * FROM "+ tableName + "";
 		PreparedStatement preparedStatement = null;
 		try {
 			preparedStatement = conn.prepareStatement(query);
-			preparedStatement.setInt(1, 1001);
+			//preparedStatement.setInt(1, 1001);
 
 			// execute select SQL stetement
 			ResultSet rs = preparedStatement.executeQuery();
 
 			
-				if(tableName == "Tbl_Renk") {
-					ArrayList<Color> colors = new ArrayList<>();
+				if(tableName.equals("Tbl_Renk")) {
+					ArrayList<Object> colors = new ArrayList<>();
 					while (rs.next()) {
 						Color color = new Color();
 						Integer renkID = rs.getInt("RenkID");
@@ -237,12 +249,12 @@ public class DatabaseConnector {
 						color.setColorID(renkID);
 						color.setColorName(renkName);
 						colors.add(color);
-						
 					}
+					return colors;
 				}
 				
-				if(tableName == "Tbl_VitesTuru") {
-					ArrayList<Transmission> transmissions = new ArrayList<>();
+				if(tableName.equals("Tbl_VitesTuru")) {
+					ArrayList<Object> transmissions = new ArrayList<>();
 					while (rs.next()) {
 						Transmission transmission = new Transmission();
 						Integer transmissionID = rs.getInt("VitesTuruID");
@@ -250,12 +262,13 @@ public class DatabaseConnector {
 						transmission.setTransmissionID(transmissionID);
 						transmission.setTransmissionType(transmissionType);
 						transmissions.add(transmission);
-						
 					}
+					return transmissions;
+
 				}
 				
-				if(tableName == "Tbl_Sehir") {
-					ArrayList<City> cities= new ArrayList<>();
+				if(tableName.equals("Tbl_Sehir")) {
+					ArrayList<Object> cities= new ArrayList<>();
 					while (rs.next()) {
 						City city = new City();
 						Integer cityID = rs.getInt("SehirID");
@@ -263,21 +276,52 @@ public class DatabaseConnector {
 						city.setCityID(cityID);
 						city.setCityName(cityName);
 						cities.add(city);
-						
 					}
+					return cities;
+
 				}
 				
-				if(tableName == "Tbl_YakitTuru") {
-					ArrayList<Fuel> fuels= new ArrayList<>();
+				if(tableName.equals("Tbl_YakitTuru")) {
+					ArrayList<Object> fuels= new ArrayList<>();
 					while (rs.next()) {
 						Fuel fuel= new Fuel();
 						Integer fuelID = rs.getInt("YakitTuruID");
-						String fuelType= rs.getString("YakitTuruID");
+						String fuelType= rs.getString("YakitTuru");
 						fuel.setFuelID(fuelID);
 						fuel.setFuelType(fuelType);
 						fuels.add(fuel);
-						
 					}
+					return fuels;
+
+				}
+				if(tableName.equals("Tbl_Araba")) {
+					ArrayList<Object> cars= new ArrayList<>();
+					while (rs.next()) {
+						Car car= new Car();
+						Integer carID = rs.getInt("ArabaID");
+						String carBrand= rs.getString("Araba_Marka");
+						String carModel= rs.getString("Araba_Model");
+						Integer transmissionTypeID= rs.getInt("Araba_VitesTuruID");
+						Integer fuelTypeID= rs.getInt("Araba_YakitTuruID");
+						Integer colorID = rs.getInt("Araba_RenkID");
+						String transmissionType = selectCertainData(conn, "Tbl_VitesTuru", "VitesTuru", "VitesTuruID", transmissionTypeID);
+						String fuelType = selectCertainData(conn, "Tbl_YakitTuru", "YakitTuru", "YakitTuruID", fuelTypeID);
+						String colorName = selectCertainData(conn, "Tbl_Renk", "Renk", "RenkID", colorID);
+
+						car.setCarID(carID);
+						car.setCarBrand(carBrand);
+						car.setCarModel(carModel);
+						car.setTransmissionTypeID(transmissionTypeID);
+						car.setFuelTypeID(fuelTypeID);
+						car.setColorID(colorID);
+						car.setTransmissionType(transmissionType);
+						car.setFuelType(fuelType);
+						car.setColorName(colorName);
+
+						cars.add(car);
+					}
+					return cars;
+
 				}
 				
 				
@@ -300,19 +344,78 @@ public class DatabaseConnector {
 			}
 
 		}
+		
+		return null;
 
 	}
     
-    public void insertRecord(Connection connection,String name) {
+    public void insertRecord(Connection connection,String tableName,String data) {
     	Statement statement = null;
     	String insertTableSQL = "sa";
-    //	if(button.equals("btnSave")) {
+    	if(tableName.equals("Tbl_YakitTuru")) {
     		insertTableSQL = "INSERT INTO Tbl_YakitTuru"
 				+ "(YakitTuru) " 
     			+ "VALUES"
-				+ "('"+ name + "')";
-    	//}
-		
+				+ "('"+ data + "')";
+    	}
+    	
+    	if(tableName.equals("Tbl_Renk")) {
+    		insertTableSQL = "INSERT INTO Tbl_Renk"
+				+ "(Renk) " 
+    			+ "VALUES"
+				+ "('"+ data + "')";
+    	}
+    	if(tableName.equals("Tbl_Sehir")) {
+    		insertTableSQL = "INSERT INTO Tbl_Sehir"
+				+ "(Sehir) " 
+    			+ "VALUES"
+				+ "('"+ data + "')";
+    	}
+    	
+    	if(tableName.equals("Tbl_VitesTuru")) {
+    		insertTableSQL = "INSERT INTO Tbl_VitesTuru"
+				+ "(VitesTUru) " 
+    			+ "VALUES"
+				+ "('"+ data + "')";
+    	}
+
+		try {
+			statement = connection.createStatement();
+
+			System.out.println(insertTableSQL);
+
+			statement.executeUpdate(insertTableSQL);
+
+			System.out.println("Record was inserted into table.");
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+    }
+    
+
+    public void insertRecord(Connection connection,String tableName,String[] datas) {
+    	Statement statement = null;
+    	String insertTableSQL = "sa";
+    	if(tableName.equals("Tbl_Araba")) {
+    		insertTableSQL = "INSERT INTO Tbl_Araba"
+				+ "(Araba_Marka,Araba_Model,Araba_VitesTuruID,Araba_YakitTuruID,Araba_RenkID)"
+    			+ "VALUES"
+				+ "('"+ datas[0] + "','" + datas[1] + "',(SELECT VitesTuruID from Tbl_VitesTuru WHERE VitesTuru = '" + datas[2] + "'),(SELECT YakitTuruID from Tbl_YakitTuru WHERE YakitTuru = '" + datas[3] + "'),(SELECT RenkID from Tbl_Renk WHERE Renk = '" + datas[4] + "'))";
+    	}
+    	
 
 		try {
 			statement = connection.createStatement();
@@ -342,8 +445,25 @@ public class DatabaseConnector {
     
     public void deleteRecord(Connection connection,String ID,String table) {
     	Statement statement = null;
-
-		String deleteTableSQL = "DELETE FROM "  + table + " WHERE YakitTuruID = " + Integer.parseInt(ID);
+    	String deleteTableSQL = ".";
+    	if(table.equals("Tbl_YakitTuru")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE YakitTuruID = " + Integer.parseInt(ID);
+    	}
+    	if(table.equals("Tbl_VitesTuru")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE VitesTuruID = " + Integer.parseInt(ID);
+    	}
+    	if(table.equals("Tbl_Sehir")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE SehirID = " + Integer.parseInt(ID);
+    	}
+    	if(table.equals("Tbl_Renk")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE RenkID = " + Integer.parseInt(ID);
+    	}
+    	if(table.equals("Tbl_Araba")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE ArabaID = " + Integer.parseInt(ID);
+    	}
+    	if(table.equals("Tbl_Ilan")) {
+    		deleteTableSQL =  "DELETE FROM "  + table + " WHERE IlanID = " + Integer.parseInt(ID);
+    	}
 
 		try {
 			statement = connection.createStatement();
@@ -373,4 +493,198 @@ public class DatabaseConnector {
 		}
 
     }
+    
+    public void updateRecord(Connection connection, String id, String tableName,Integer row, DefaultTableModel dtm) {
+    	if(tableName.equals("Tbl_YakitTuru")) {
+    			try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE "+ tableName +" SET YakitTuru = ? WHERE YakitTuruID = "+id);
+    
+             ps.setString(1, (String) dtm.getValueAt(row, 1));
+   
+             ps.executeUpdate(); 
+             ps.close();
+   
+         } catch (Exception ex) {
+             System.out.println("An error occured");
+         }
+    	}
+    	
+       	if(tableName.equals("Tbl_Renk")) {
+			try {
+        PreparedStatement ps = connection.prepareStatement("UPDATE "+ tableName +" SET Renk = ? WHERE RenkID = "+id);
+         ps.setString(1, (String) dtm.getValueAt(row, 1));
+
+         ps.executeUpdate(); 
+         ps.close();
+
+     } catch (Exception ex) {
+       System.out.println("An error occured");
+     }
+	}
+       	
+      	if(tableName.equals("Tbl_Sehir")) {
+    			try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE "+ tableName +" SET Sehir = ? WHERE SehirID = "+id);
+             ps.setString(1, (String) dtm.getValueAt(row, 1));
+
+             ps.executeUpdate(); 
+             ps.close();
+
+         } catch (Exception ex) {
+           System.out.println("An error occured");
+         }
+    	}
+      	
+    	if(tableName.equals("Tbl_VitesTuru")) {
+			try {
+        PreparedStatement ps = connection.prepareStatement("UPDATE "+ tableName +" SET VitesTuru = ? WHERE VitesTuruID = "+id);
+         ps.setString(1, (String) dtm.getValueAt(row, 1));
+
+         ps.executeUpdate(); 
+         ps.close();
+
+     } catch (Exception ex) {
+       System.out.println("An error occured");
+     }
+	}
+    
+    }
+    
+    public ArrayList<String> getColorNames(Connection connection){
+    	ArrayList<String> colorNames = new ArrayList<String>();
+    	String query = "SELECT Renk FROM Tbl_Renk ORDER BY Renk"; 
+    	PreparedStatement stm;
+    	ResultSet rs = null;
+		try {
+			stm = connection.prepareStatement(query);
+		
+
+    	 rs = stm.executeQuery(query); 
+
+    	while (rs.next()) { 
+    	    String colorName = rs.getString("Renk"); 
+    	    // add group names to the array list
+    	    colorNames.add(colorName);
+    	} 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			    	try {
+						rs.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+
+		}
+
+    	
+    	
+    	return colorNames;
+    }
+    
+    public ArrayList<String> getTransmissionTypes(Connection connection){
+    	ArrayList<String> transmissionTypes = new ArrayList<String>();
+    	String query = "SELECT VitesTuru FROM Tbl_VitesTuru ORDER BY VitesTuru"; 
+    	PreparedStatement stm;
+    	ResultSet rs = null;
+		try {
+			stm = connection.prepareStatement(query);
+		
+
+    	 rs = stm.executeQuery(query); 
+
+    	while (rs.next()) { 
+    	    String transmissionType = rs.getString("VitesTuru"); 
+    	    // add group names to the array list
+    	    transmissionTypes.add(transmissionType);
+    	} 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			    	try {
+						rs.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+
+		}
+
+    	
+    	
+    	return transmissionTypes;
+    }
+    
+    
+    public ArrayList<String> getFuelTypes(Connection connection){
+    	ArrayList<String> fuelTypes = new ArrayList<String>();
+    	String query = "SELECT YakitTuru FROM Tbl_YakitTuru ORDER BY YakitTuru"; 
+    	PreparedStatement stm;
+    	ResultSet rs = null;
+		try {
+			stm = connection.prepareStatement(query);
+		
+
+    	 rs = stm.executeQuery(query); 
+
+    	while (rs.next()) { 
+    	    String fuelType = rs.getString("YakitTuru"); 
+    	    // add group names to the array list
+    	    fuelTypes.add(fuelType);
+    	} 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			    	try {
+						rs.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+
+		}
+
+    	
+    	
+    	return fuelTypes;
+    }
+    
+    
+    public String selectCertainData(Connection connection, String tableName, String dataName, String IDName, Integer ID) {
+    	String data = "problem:)";
+    	String query = "SELECT " + dataName + " FROM " + tableName + " WHERE " + IDName + " = " + ID; 
+    	PreparedStatement stm;
+    	ResultSet rs = null;
+		try {
+			stm = connection.prepareStatement(query);
+		
+
+    	 rs = stm.executeQuery(query); 
+
+    	while (rs.next()) { 
+    	    data= rs.getString(dataName); 
+    	    // add group names to the array list
+    	} 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			    	try {
+						rs.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+
+		}
+
+    	
+    	
+    	return data;
+    }
+    
     }
